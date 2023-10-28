acd47f918e88eedba90cc2d7bef6bec394424441
Ridvansezen <sezenridvan309@gmail.com>
2023-10-28 23:41:33 +0300

    Python Mutable-Immutable tutorial.

---
+++ Mutable-Immutable/immutable.py
@@ -0,0 +1,14 @@
+""""Python da Immutable (Değiştirilemez) veri tipi."""
+
+# Immutable (Değiştirilemez) veri tipi (string).
+my_string = "Hello"
+
+my_string += " World" # Veri değiştirilemez, ancak yeni bir veri oluşturabilirsiniz.
+
+print(my_string)
+
+# ---------------------------------------------
+
+my_tuple = (1,2,3)
+
+# my_tuple.append(4) # Hata! Tuple veri tipi değiştirilemez.
---
+++ Mutable-Immutable/mutable.py
@@ -0,0 +1,8 @@
+""""Python da Mutable (Değiştirilebilir) veri tipi."""
+
+# Mutable (değiştirilebilir) veri tipi (liste).
+my_list = [1,2,3]
+
+my_list.append(4) # Listeye yeni bir veri ekliyoruz.
+
+print(my_list) # [1, 2, 3, 4]
